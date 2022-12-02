c835ad430c150e76a7c1d929514fcfee4f004f0b
= <=>
2022-12-02 07:17:01 -0300

    Commit Database

---
+++ conf/database.py
@@ -0,0 +1,9 @@
+import mysql.connector;
+class ConObj:
+    def __init__(self):
+        self.host="localhost",
+        self.user="wallace",
+        self.password="522345",
+        self.database="semed_manaus";
+
+
